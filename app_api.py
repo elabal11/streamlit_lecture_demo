dea816331e095c7f70aa0b00e596f54da0a428bb
yassine laghrari <laghrari.yassine@gmail.com>
2022-02-25 00:40:24 +0100

    api call

--- app_api.py
+++ app_api.py
@@ -3,12 +3,15 @@ import requests
 
 def app():
     st.title("Streamlit Lecture")
-    input = st.text_input("Input your text")
+
+    st.header("Text in Image Api")
+    input = st.text_input("Input your text For Bruzu Api")
     if input:
         url = f"https://img.bruzu.com/?backgroundImage=https://source.unsplash.com/U-Kty6HxcQc/500x500%20&a.text={input}&a.color=white&a.fontFamily=Poppins&a.fontWeight=800&a.width=450&b.text=%20@naval&b.width=450&b.top=480&b.originY=bottom&b.color=white&b.fontFamily=Playfair%20Display&b.fontSize=30"
         st.image(requests.get(url).content)
 
-    button = st.button("Get Random images from Picsum API")
+    st.header("Picsum API")
+    button = st.button("Get Random images")
     if button:
-        url = f"https://picsum.photos/200"
+        url = f"https://picsum.photos/500"
         st.image(requests.get(url).content)
