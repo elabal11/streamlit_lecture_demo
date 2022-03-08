1ce55b0455fcd9288a85f387fdc823fcfb982e1c
yassine laghrari <laghrari.yassine@gmail.com>
2022-03-08 15:37:04 +0000

    image slider

--- app_image.py
+++ app_image.py
@@ -3,10 +3,24 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
+img_list = ['abstract-red-color-light-oblique-1630900.jpeg','black-white-wallpaper-11.jpeg','grapefruit-slice-332-332.jpeg','images.jpeg']
+
 def app():
+    st.title("Streamlit Lecture")
     uploaded_file = st.file_uploader('Upload an image',type=['jpeg','jpg','png'])
+
+
 
     if uploaded_file:
         im = Image.open(uploaded_file)
-        st.image(im)
-        st.balloons()
+        col1, col2 = st.columns(2)
+
+        img = Image.open(img_list[0])
+
+        col2.image(im, use_column_width=True)
+
+        slider = st.slider('train GAN',0,len(img_list)-1,step=1,)
+        if not slider:
+            slider = 0 # Default Value
+        img = Image.open(img_list[slider])
+        col1.image(img, use_column_width=True)
--- app_main.py
+++ app_main.py
@@ -2,16 +2,48 @@
 import app
 import app_image
 import app_api
+import base64
 
 import streamlit as st
+
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg");
+background-size: cover;
+}
+</style>
+'''
+
+
 PAGES = {
-    "DataFrame Example": app,
     "Image Example": app_image,
+    "DataFrame Example": app,
     "API Example": app_api
 }
 
 st.title("Streamlit Lecture")
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
+# @st.cache(allow_output_mutation=True)
+# def get_base64_of_bin_file(bin_file):
+#     with open(bin_file, 'rb') as f:
+#         data = f.read()
+#     return base64.b64encode(data).decode()
+
+# bin_str = get_base64_of_bin_file('grapefruit-slice-332-332.jpeg')
+# CSS = """
+# .stApp {
+#     background-image: url("data:image/png;base64,%s");
+#     background-repeat: no-repeat;
+#     background-position: top;
+#     background-size: cover;
+#     background-size: 1580px 111px;
+# }
+# """ % bin_str
+# st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+
+#set_png_as_page_bg('grapefruit-slice-332-332.jpeg')
 
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
